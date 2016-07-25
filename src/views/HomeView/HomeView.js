35a4560476a3bfac3bc5df379242e7555d59810a
Ruben Garcia <rubengarciamohedano@gmail.com>
2016-07-25 19:36:59 +1000

    updated start page

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -3,6 +3,7 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { increment, doubleAsync } from '../../redux/modules/counter'
 import DuckImage from './Duck.jpg'
+import HackWeekImage from './hackweek.png'
 import classes from './HomeView.scss'
 
 // We can use Flow (http://flowtype.org/) to type our component's props
@@ -35,46 +36,30 @@ export class HomeView extends React.Component<void, Props, void> {
   render () {
     return (
       <div className='container text-center'>
-        <div className='row'>
-          <div className='col-xs-2 col-xs-offset-5'>
-            <img className={classes.duck}
-              src={DuckImage}
-              alt='This is a duck, because Redux.' />
-          </div>
-        </div>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>
-          Sample Counter:
-          {' '}
-          <span className={classes['counter--green']}>{this.props.counter}</span>
-        </h2>
-        <button className='btn btn-default' onClick={this.props.increment}>
-          Increment
-        </button>
-        {' '}
-        <button className='btn btn-default' onClick={this.props.doubleAsync}>
-          Double (Async)
-        </button>
+        <h1>Dropbox Content Discoverer</h1>
+
+        <div className="ui centered cards">
         <div className="ui card">
           <div className="image">
-              <img src="http://semantic-ui.com/images/avatar2/large/kristy.png" />
+              <img src={HackWeekImage} />
             </div>
             <div className="content">
-              <a className="header">Dropbox Sydney</a>
+              <a className="header">Dropbox HackWeek</a>
               <div className="meta">
-                <span className="date">Joined in 2013</span>
+                <span className="date">July 2016</span>
               </div>
               <div className="description">
-                Kristy is an art director living in New York.
+                Improving content discovery in Dropbox through search and intelligence
               </div>
             </div>
             <div className="extra content">
               <a>
                 <i className="user icon"></i>
-                22 Friends
+                7 Dropboxers working
               </a>
             </div>
          </div>
+          </div>
       </div>
     )
   }
@@ -87,3 +72,27 @@ export default connect((mapStateToProps), {
   increment: () => increment(1),
   doubleAsync
 })(HomeView)
+
+/*
+<h2>
+  Sample Counter:
+  {' '}
+  <span className={classes['counter--green']}>{this.props.counter}</span>
+</h2>
+<button className='ui primary button' onClick={this.props.increment}>
+  Increment
+</button>
+{' '}
+<button className='btn btn-default' onClick={this.props.doubleAsync}>
+  Double (Async)
+</button>
+
+<div className='row'>
+  <div className='col-xs-2 col-xs-offset-5'>
+    <img className={classes.duck}
+      src={DuckImage}
+      alt='This is a duck, because Redux.' />
+  </div>
+</div>
+
+*/
