53d3b190b744598459e73d6ebaece2d65240a822
Ruben Garcia <rubengarciamohedano@gmail.com>
2016-07-26 14:22:56 +1000

    continue merge

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -2,9 +2,10 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { increment, doubleAsync } from '../../redux/modules/counter'
-// import DuckImage from './Duck.jpg'
 import HackWeekImage from './hackweek.png'
-// import classes from './HomeView.scss'
+
+/* import DuckImage from './Duck.jpg'
+import classes from './HomeView.scss' */
 
 // We can use Flow (http://flowtype.org/) to type our component's props
 // and state. For convenience we've included both regular propTypes and
@@ -36,30 +37,54 @@ export class HomeView extends React.Component<void, Props, void> {
   render () {
     return (
       <div className='container text-center'>
-        <h1>Dropbox Content Discoverer</h1>
-
-        <div className="ui centered cards">
-          <div className="ui card">
-            <div className="image">
-              <img src={HackWeekImage} />
-            </div>
-            <div className="content">
-              <a className="header">Dropbox HackWeek</a>
-              <div className="meta">
-                <span className="date">July 2016</span>
+          <div className="ui icon input">
+            <input type="text" placeholder="Presentations shared by @ruben in the last week..." />
+            <i className="search icon"></i>
+          </div>
+          <div className="ui feed">
+            <div className="event">
+              <div className="label">
+                <i className="file powerpoint outline icon"></i>
               </div>
-              <div className="description">
-                Improving content discovery in Dropbox through search and intelligence
+              <div className="content">
+                  <div className="summary">
+                  <span className="file">File 1.ppt</span> Created by <a>Jenny Hess</a> in the <a>marketing</a> shared folder
+                    <div className="date">
+                      3 days ago
+                    </div>
+                </div>
               </div>
+              <div className="ui right floated primary button">Open</div>
             </div>
-            <div className="extra content">
-              <a>
-                <i className="user icon"></i>
-                7 Dropboxers working
-              </a>
+            <div className="event">
+              <div className="label">
+                <i className="file text outline icon"></i>
+              </div>
+              <div className="content">
+                  <div className="summary">
+                  <span className="file">File 1.txt</span> Created by <a>you</a> in the <a>hackweek 2016</a> shared folder
+                    <div className="date">
+                      2 weeks ago
+                    </div>
+                </div>
+              </div>
+              <div className="ui right floated primary button">Open</div>
+            </div>
+            <div className="event">
+              <div className="label">
+                <i className="file pdf outline icon"></i>
+              </div>
+              <div className="content">
+                  <div className="summary">
+                  <span className="file">File 1.pdf</span> Created by <a>Jenny Hess</a> in the <a>marketing</a> shared folder
+                    <div className="date">
+                      3 days ago
+                    </div>
+                </div>
+              </div>
+              <div className="ui right floated primary button">Open</div>
             </div>
           </div>
-        </div>
       </div>
     )
   }
@@ -96,3 +121,28 @@ export default connect((mapStateToProps), {
 </div>
 
 */
+
+/*
+<div className="ui centered cards">
+  <div className="ui card">
+    <div className="image">
+      <img src={HackWeekImage} />
+    </div>
+    <div className="content">
+      <a className="header">Dropbox HackWeek</a>
+      <div className="meta">
+        <span className="date">July 2016</span>
+      </div>
+      <div className="description">
+        Improving content discovery in Dropbox through search and intelligence
+      </div>
+    </div>
+    <div className="extra content">
+      <a>
+        Made with <i className="heart icon"></i> by 5 Dropboxers
+      </a>
+    </div>
+  </div>
+</div>
+*/
+
