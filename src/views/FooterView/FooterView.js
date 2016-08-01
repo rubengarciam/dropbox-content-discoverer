690082aa5689e6c99f084d1e4c8ce476f736b7da
Ruben Garcia <rubengarciamohedano@gmail.com>
2016-08-01 14:17:32 +1000

    favicon

--- src/views/FooterView/FooterView.js
+++ src/views/FooterView/FooterView.js
@@ -5,7 +5,10 @@ export class FooterView extends React.Component {
   render () {
     return (
       <div className="ui vertical footer text-center segment">
-        Dropbox HackWeek July 2016, Improving content discovery in Dropbox through search and intelligence.
+        Dropbox HackWeek July 2016
+        <a href="https://github.com/rubengarciam/dropbox-content-discoverer">
+          - Improving content discovery in Dropbox through search and intelligence  -
+        </a>
         Made with <i className="heart icon"></i> by a bunch of Dropboxers
       </div>
     )
